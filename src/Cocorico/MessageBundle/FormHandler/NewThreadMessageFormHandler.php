0e38601f6cc4e1bfcef0ef83b63a20680c40c25b
Ismailmehri <ismail.mehri@gmail.com>
2021-01-18 22:53:11 +0100

    Update for Jobber

--- src/Cocorico/MessageBundle/FormHandler/NewThreadMessageFormHandler.php
+++ src/Cocorico/MessageBundle/FormHandler/NewThreadMessageFormHandler.php
@@ -19,6 +19,8 @@ use FOS\MessageBundle\Model\MessageInterface;
 class NewThreadMessageFormHandler extends AbstractMessageFormHandler
 {
 
+    const BASE_URL = 'http://api.smspartner.fr/v1/';
+
     /**
      * Composes a message from the form data
      *
@@ -41,7 +43,66 @@ class NewThreadMessageFormHandler extends AbstractMessageFormHandler
             ->setSender($this->getAuthenticatedParticipant())
             ->setBody($message->getBody());
 
+
+            $fields = array(
+                "apiKey"=>"2c97113671c894d9995f31734670feb80b183d6d",
+                "phoneNumbers"=>"+21621170468",
+                "message"=> "Test",
+                "sender" => "Jobber"
+            
+            );
+        // $this->sendSms($fields);
         return $newThread->getMessage();
     }
+
+	public function sendSms($fields)
+	{
+		if (empty($fields))
+			return false;
+
+		$result = $this->postRequest(self::BASE_URL.'send', $fields);
+		return $this->returnJson($result);
+	}
+
+	/**
+	 * Requête cURL - Vous n'êtes pas sensé appeler cette méthode
+	 * @access private
+	 *
+	 */
+	private function postRequest($url, $fields = array())
+	{
+		$curl = curl_init();
+		curl_setopt($curl, CURLOPT_URL, $url);
+		curl_setopt($curl, CURLOPT_RETURNTRANSFER, true);
+		curl_setopt($curl, CURLOPT_TIMEOUT, 30);
+		if (!empty($fields))
+		{
+
+			curl_setopt($curl, CURLOPT_POST, true);
+			curl_setopt($curl, CURLOPT_POSTFIELDS, json_encode($fields));
+		}
+
+		$result = curl_exec($curl);
+
+		if ($result === false)
+		{
+			if ($this->debug)
+				echo curl_error($curl);
+			else
+				$result = curl_error($curl);
+		}
+		else
+			curl_close($curl);
+		return $result;
+	}
+
+	private function returnJson($string)
+	{
+		$json_array = json_decode($string);
+		if (is_null($json_array))
+			return $string;
+		else
+			return $json_array;
+	}
 
 }
